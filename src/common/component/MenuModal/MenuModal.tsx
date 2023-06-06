5d9578b8f456b6da47508a1b109abd8ef81242c3
Alexsus <alexsuslin@inbox.ru>
2023-06-06 21:07:48 +0300

    add menuModal

--- src/common/component/MenuModal/MenuModal.tsx
+++ src/common/component/MenuModal/MenuModal.tsx
@@ -1,9 +1,20 @@
 import s from "./menuModal.module.scss";
+import user from "../../image/user.svg";
+import logout from "../../image/logout2.svg";
 export const MenuModal = () => {
+  const iconStyle = { width: "14px", height: "14px" };
   return (
     <div className={s.modal}>
-      <div> Profile</div>
-      <div> Logout</div>
+      <div>
+        <span className={s.str}>
+          <img style={iconStyle} src={user} alt="" /> Profile
+        </span>
+      </div>
+      <div>
+        <span className={s.str}>
+          <img style={iconStyle} src={logout} alt="" /> Logout
+        </span>
+      </div>
     </div>
   );
 };
