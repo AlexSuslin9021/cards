2fd2e1f1b86a4fddeebf25f0c497bc2d1a2daad1
AlexSuslin9021 <alexsuslin@inbox.ru>
2023-06-19 10:56:59 +0300

    add image in EditableSpan.tsx

--- src/common/component/Profile/EditableSpan/EditableSpan.tsx
+++ src/common/component/Profile/EditableSpan/EditableSpan.tsx
@@ -2,8 +2,10 @@ import { ChangeEvent, useState } from "react";
 import { useAppDispatch, useAppSelector } from "common/hooks";
 import { updateUserTC } from "features/Auth/auth.slice";
 import s from "common/component/Profile/EditableSpan/EditableSpan.module.scss";
+import changeName from '../../../../common/image/pencil.svg'
 
 export const EditableSpan = () => {
+  const style={ width: "15px", height: "15px" }
   const name = useAppSelector<any>((state) => {
     if (state.auth.profile !== null) return state.auth.profile.name;
   });
@@ -24,7 +26,8 @@ export const EditableSpan = () => {
     <div>
       {editMode ? (
         <div className={s.imageContainer}>
-          <span onDoubleClick={onClickHandler}>{value}</span>
+          <span onDoubleClick={onClickHandler}>{value} </span>
+          <img src={changeName} style={style} alt="" />
         </div>
       ) : (
         <input value={value} onChange={onChangeHandler} onBlur={onBlurChange} />
